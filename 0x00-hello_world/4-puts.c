0fb1b0e1ff0a52814fe64c18bcfea1ff53db1d1a
k-warutumo <kevinwnderitu@gmail.com>
2023-02-16 12:42:59 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 vi 4-puts.c
 #include <stdio.h>
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
