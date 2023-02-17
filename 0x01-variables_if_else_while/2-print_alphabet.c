17a46d6585d90572e2e1d88a265b3b235f8013ff
k-warutumo <kevinwnderitu@gmail.com>
2023-02-17 13:37:38 -0800

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Prints the alphabet
+ * Return: Always (SUCCESS)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a', c <= 'z', c++)
+		putchar(c);
+		putchar('\n');
+	Return(0);
