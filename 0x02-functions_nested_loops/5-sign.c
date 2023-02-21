f3c3b6511873f4bd60829c78aed1ba4d90ce5034
k-warutumo <kevinwnderitu@gmail.com>
2023-02-21 11:54:30 -0800

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,12 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar(+)
-			return (1)
+		_putchar(+);
+			return (1);
 	if else(n < 0)
-		_putchar(-)
-			return (-1)
+		_putchar(-);
+			return (-1);
 	else
-		_putchar(0)
-			return (0)
+		_putchar(0);
+			return (0);
 }
