3f57d563a1567170f5458f00fd25356ce6c59898
k-warutumo <kevinwnderitu@gmail.com>
2023-02-21 11:51:45 -0800

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - print te sign of number
+ *@n: the number to be checked
+ * Return: 1 if number is + 0 if number is zero -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar(+)
+			return (1)
+	if else(n < 0)
+		_putchar(-)
+			return (-1)
+	else
+		_putchar(0)
+			return (0)
+}
