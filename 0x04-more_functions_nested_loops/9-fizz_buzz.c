568c8ada00ad49456895723dedb2d58117366b60
k-warutumo <kevinwnderitu@gmail.com>
2023-02-23 09:48:26 -0800

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - print Fizz for multiples of 3, Buzz for multiples of 5 and FizzBuzz for both
+ * main - Fizz for multiples of 3, Buzz for multiples of 5,FizzBuzz for both
  *
  *
  * Return: Always 0
