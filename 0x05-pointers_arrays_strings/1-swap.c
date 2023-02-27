3421592d3c46d7d832169be26c23c5e186d93108
k-warutumo <kevinwnderitu@gmail.com>
2023-02-27 13:24:23 -0800

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int  - function to swat integers
+ * @a: - first interger
+ * @b: - second interger
+ * 
+ */
+void swap_int(int *a, int *b)
+{
+	int d;
+	
+	d = *a;
+	*a = *b;
+	*b = d;
+}
