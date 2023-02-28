aff6371d14465cfe364e4304ee22ebe9248dbf31
k-warutumo <kevinwnderitu@gmail.com>
2023-02-28 05:12:39 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_rev - print the string in reverse
+ *@s: - string to print
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i= 0;
+
+	while (s[i])
+	i++;
+
+	while (--i)
+		_putchar(s[i]);
+	_putchar('\n');
+}
