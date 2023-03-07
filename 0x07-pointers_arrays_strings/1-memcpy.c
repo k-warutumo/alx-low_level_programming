83a8c0016255515ba7944b386c93a49703473e5d
k-warutumo <kevinwnderitu@gmail.com>
2023-03-07 05:02:08 -0800

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @s: string
+ * @n: given number of bytes
+ * @b: character
+ * @i: number of characters
+ * return: returns pointer to dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (dest)
+}
