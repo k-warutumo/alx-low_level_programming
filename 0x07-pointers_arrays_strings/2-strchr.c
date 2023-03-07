3410b6ad962a37ef0671d28f8cd97a47e004e629
k-warutumo <kevinwnderitu@gmail.com>
2023-03-07 05:55:56 -0800

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *@c: character being located
+ *@s: string to locate c in
+ * Return: returns pointer to occurence of n
+ */
+char *_strchr(char *s, char c)
+{
+	int i:
+
+		for (i = 0; s[i] >= '\0'; i++)
+		{
+			if (s[i] == c)
+			return (s + i);
+		}
+}
