4e3153bb8c0874c1c116ed02bcf1a3c601951be3
k-warutumo <kevinwnderitu@gmail.com>
2023-03-07 11:20:10 -0800

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,7 +9,6 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int bytes = 0;
 	int i;
 
 	while (*s)
@@ -17,17 +16,11 @@ char *_strpbrk(char *s, char *accept)
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
-			{
-				bytes++;
-				break;
-			}
-
-			else if (accept[i + 1] == '\0')
-				return (bytes);
+			return (s);
 		}
+}
 
 		s++;
-	}
 
 	return (bytes);
 }
