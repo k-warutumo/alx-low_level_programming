16909f09152c43bac42a73d946854eeae4ed9b57
k-warutumo <kevinwnderitu@gmail.com>
2023-03-07 05:32:15 -0800

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,20 +2,18 @@
 
 /**
  * _memcpy - copies memory area
- * @s: string
  * @n: given number of bytes
- * @b: character
- * @i: number of characters
- * @dest: destination
+ * @dest: memory area
+ * @src: source
  * Return: returns pointer to dest
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		dest[i] = src[i];
 	}
-	return (0);
+	return (dest);
 }
