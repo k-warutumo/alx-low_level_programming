5b4aac9a07e0f65970bc157973a2ef38dc96687e
k-warutumo <kevinwnderitu@gmail.com>
2023-03-08 09:25:59 -0800

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ *@s: pointer to the string
+ *
+ * Return: null
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0') /*base case*/
+	{
+		_putchar(\n); /* add anew line*/
+	return;
+	}
+	_putchar(*s); /*print the current character*/
+	_put_recursion(*s + 1); /*recurse to next character*/
+}
