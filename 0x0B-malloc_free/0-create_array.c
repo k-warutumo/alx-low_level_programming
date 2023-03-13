2cfac6344a9bc6cf1900645ad5a806be6551956b
k-warutumo <kevinwnderitu@gmail.com>
2023-03-13 09:41:15 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,8 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+
 	if (size == 0)
 	{
 		return (NULL);
@@ -21,6 +23,9 @@ char *create_array(unsigned int size, char c)
 	{
 	return (NULL);
 	}
+
+	unsigned int i;
+
 	for (unsigned int i = 0; i < size; i++)
 	{
 	array[i] = c;
