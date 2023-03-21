94099b4013497a7e0f2b538917ff638b1f1e9c94
k-warutumo <kevinwnderitu@gmail.com>
2023-03-20 23:30:36 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-	struct dog
-	{
-		char name;
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Dexcription: this struct is for dog
+ */
+
+struct dog
+{
+		char *name;
 		float age;
-		char owner;
-	};
+		char *owner;
+};
+
 #endif
