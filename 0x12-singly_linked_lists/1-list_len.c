69e483d9bf206755d0207ef3cd6dbcaaca9c7479
k-warutumo <kevinwnderitu@gmail.com>
2023-03-31 13:02:56 -0700

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+
+/**
+ *list_len - counts the no of elements in the list
+ *@h: linked list
+ *Return: number of elements in the list
+ *
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;/*initialize count to zero*/
+
+	while (h != NULL)
+	{
+		count++;/*increment count for each node*/
+		h->next;/*move to next node*/
+	}
+	return (count);
+}
