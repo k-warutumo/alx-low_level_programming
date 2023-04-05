acdd304ce6cf742620f7bca60343e547cbb69bd2
k-warutumo <kevinwnderitu@gmail.com>
2023-04-05 08:56:52 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+
+/**
+ *free_listint - funtion that frees a list
+ *@head: pointer to the head node
+ *
+ *Return: nothing to return
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+
+}
