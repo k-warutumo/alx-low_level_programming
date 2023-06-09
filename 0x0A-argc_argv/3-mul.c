a1c5e59999aa096085084b53e5a193a235660f9a
k-warutumo <kevinwnderitu@gmail.com>
2023-06-09 14:30:34 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  *main -multiplies 2 numbers
@@ -13,7 +14,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error\n");
+	printf("Error\n");
 	return (1);
 	}
 
