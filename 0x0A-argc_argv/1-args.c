4e3d07a921c29378c5076cc6152367b455d6369c
k-warutumo <kevinwnderitu@gmail.com>
2023-06-09 12:51:36 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ *main - prints
+ *@argc: no .o argument
+ *@argv: array of pointer
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	/*prints no. of arguments*/
+	printf("%d\n", argc);
+
+	return (0);
+}
