3ba84a5a52af2131094920c2312b2d94988ad828
k-warutumo <kevinwnderitu@gmail.com>
2023-06-09 12:10:14 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *main - prints the prog name
+ *@argc: counts number of arguments
+ *@argv: array of pointers of argc
+ *Return: 0
+ */
+
+int main(int argc, char argv[])
+{
+	printf("%s/n", agrv[argc * 0]);
+
+	return (0);
+}
