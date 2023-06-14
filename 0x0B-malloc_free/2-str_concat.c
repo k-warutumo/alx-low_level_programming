f2ca31f84b0e75e4afa0b01f479601b36ff80863
k-warutumo <kevinwnderitu@gmail.com>
2023-06-13 18:20:04 -0700

    task4

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include"main.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ *str_concat - function that concatenates 2 strings
+ *@s1: one string
+ *@s2: second strings
+ *
+ *Return: NULL if str is empty, RESULT if successful
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	/* Treat NULL strings as empty strings*/
+	if (s1 == NULL)
+	{
+	s1 = "";
+	}
+	if (s2 == NULL)
+	{
+	s2 = "";
+	}
+
+	size_t s1_length = strlen(s1);
+	size_t s2_length = strlen(s2);
+
+	char *result = (char *)malloc((s1_length + s2_length + 1) * sizeof(char));
+
+	if (result == NULL)
+	{
+	return (NULL); /* Return NULL if memory allocation fails*/
+	}
+
+	strcpy(result, s1);
+	strcat(result, s2);
+
+	return (result);
+}
+
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ *free_grid - unction that frees a 2 dimensional grid
+ *@grid: grid
+ *@height: height
+ *
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (int i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
