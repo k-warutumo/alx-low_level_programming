0fc7a8bfc5e1f85b39fe62dea5497da8109b2383
k-warutumo <kevinwnderitu@gmail.com>
2023-06-19 07:29:08 -0700

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *init_dog - funtion that initialises dog.h
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of the dog
+ *@d: the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner
+	}
+}
