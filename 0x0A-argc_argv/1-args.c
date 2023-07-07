ea9ed49eb2aa3adaa7724019fb2c938b72531fd7
k-warutumo <kevinwnderitu@gmail.com>
2023-07-07 13:08:17 -0700

    tas1 re done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- *main - prints
+ *main - prints no. of arguments passed to it
  *@argc: no .o argument
  *@argv: array of pointer
  *Return: 0
@@ -10,10 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	int number = argc - 1;
+	int i;
 
-	/*prints no. of arguments*/
-	printf("%d\n", number);
+	if (argc == 1)
+		printf("%d/n", argc - 1);
+
+	else
+	{
+		for (i = 0; *argv; i++, argv++);
+
+		printf("%d/n", i - 1);
+	}
+	
 
 	return (0);
 }
