6c494156226c1b0fadbf5f22bac14420525b0c76
k-warutumo <kevinwnderitu@gmail.com>
2023-07-10 10:20:44 -0700

    thrid trail

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - function that prints number of arguments passed to it
+ *@argc: number of commands
+ *@argv: array that contains arguments
+ *Return: returns count
+ *
+ */
+
+
+int main(int argc, char *argv[])
+{
+/* Print the number of arguments.*/
+	printf("%d\n", argc);
+
+	return (0);
+}
