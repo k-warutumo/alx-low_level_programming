d3383d4f2336edf0f651151751ff060d56eb3ab9
k-warutumo <kevinwnderitu@gmail.com>
2023-07-12 14:41:46 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- *main - function that prints 2 numbers
- *@argc: number of commands
- *@argv: array containg commands
- *Return: 0 if successs, 1 if fails
+ * main - function multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ * Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
-/* Check if the program received two arguments.*/
 	if (argc != 3)
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
-
-	int product = (atoi(argv[1]) * atoi(argv[2]));
-	/* Print the result of the multiplication.*/
-	printf("%d\n", product);
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
