ffbd08954d6611e7450aac86788413967e0ac721
k-warutumo <kevinwnderitu@gmail.com>
2023-07-12 13:43:04 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,40 +1,39 @@
-#include"main.h"
-#include<stdlib.h>
-#include<string.h>
+#include "main.h"
+#include <stdlib.h>
 
 /**
- *str_concat - function that concatenates 2 strings
- *@s1: one string
- *@s2: second strings
+ * str_concat - function that Concatenates two strings.
+ * @s1: The initial string.
+ * @s2: The string to be concatenated to s1.
  *
- *Return: NULL if str is empty, RESULT if successful
+ * Return: If concatenation fails - NULL.
+ *         else return  a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	/* Treat NULL strings as empty strings*/
+	char *concat;
+	int i, concat_i = 0, len = 0;
+
 	if (s1 == NULL)
-	{
-	s1 = "";
-	}
+		s1 = "";
+
 	if (s2 == NULL)
-	{
-	s2 = "";
-	}
+		s2 = "";
 
-	size_t s1_length = strlen(s1);
-	size_t s2_length = strlen(s2);
+	for (i = 0; s1[i] || s2[i]; i++)
+		len++;
 
-	char *result = (char *)malloc((s1_length + s2_length + 1) * sizeof(char));
+	concat = malloc(sizeof(char) * len);
 
-	if (result == NULL)
-	{
-	return (NULL); /* Return NULL if memory allocation fails*/
-	}
+	if (concat == NULL)
+		return (NULL);
 
-	strcpy(result, s1);
-	strcat(result, s2);
+	for (i = 0; s1[i]; i++)
+		concat[concat_i++] = s1[i];
 
-	return (result);
+	for (i = 0; s2[i]; i++)
+		concat[concat_i++] = s2[i];
+
+	return (concat);
 }
-
