8080d98d015156fb8dedc48a25d61bc7ed66cc6f
k-warutumo <kevinwnderitu@gmail.com>
2023-07-12 13:58:16 -0700

    task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,12 @@
 #include"main.h"
 #include<stdlib.h>
-#include<stdio.h>
+
 
 /**
- *free_grid - unction that frees a 2 dimensional grid
+ *free_grid - function that frees a 2 dimensional grid
  *@grid: grid
  *@height: height
- *
+ *Return: no return
  *
  */
 
