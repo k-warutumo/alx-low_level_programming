eb8308dfcbabdd102bb53673b10222b88c27602d
k-warutumo <kevinwnderitu@gmail.com>
2023-07-12 13:53:33 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,43 +1,41 @@
 #include"main.h"
 #include<stdlib.h>
-#include<stdio.h>
-
 /**
- *alloc_grid - returns pointer
- *@width: array width
- *@height: array height
- *Return: NULL if either is 0
+ * alloc_grid - function that returns a pointer to a 2D array of integers
+ * @width: width of the array
+ * @height: height of the array
+ * Return: pointer of an array of integers
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	
-	if (width <= 0 || height <= 0)
+	int **area;
+	int x, y;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	area = malloc(height * sizeof(int *));
+	if (area == NULL)
 	{
-	return (NULL);
+		free(area);
+		return (NULL);
 	}
 
-	/* Declare the grid variable inside the for loop*/
-	for (int i = 0; i < height; i++)
+	for (x = 0; x < height; x++)
 	{
-	int **grid = calloc(width, sizeof(int *));
-
-	if (grid == NULL)
-	{
-	for (int j = 0; j < i; j++)
-	{
-	free(grid[j]);
-	}
-	free(grid);
-	return (NULL);
+		area[x] = malloc(width * sizeof(int));
+		if (area[x] == NULL)
+		{
+			for (x--; x >= 0; x--)
+				free(area[x]);
+			free(area);
+			return (NULL);
+		}
 	}
 
-	for (int j = 0; j < width; j++)
-	{
-		grid[i][j] = 0;
-	}
-	}
+	for  (x = 0; x < height; x++)
+		for (y = 0; y < width; y++)
+			area[x][y] = 0;
 
-	return (grid);
+	return (area);
 }
